decdd9445fd89bb84214cc85cb4fcf3cd5e86010
Aleksandra Khvan <aleksandra.klochko@t-systems.com>
2021-06-27 15:46:31 +0300

    logging was added

--- src/main/java/de/telekom/sea3/spring/PersonRepositoryDB.java
+++ src/main/java/de/telekom/sea3/spring/PersonRepositoryDB.java
@@ -1,6 +1,7 @@
 package de.telekom.sea3.spring;
 
 import de.telekom.sea3.spring.model.Person;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,6 @@ import java.util.Optional;
 
 @Repository
 public interface PersonRepositoryDB extends CrudRepository<Person, Long> {
-
+    @Query(value="SELECT * from persons",nativeQuery=true )
+    Iterable<Person> selectPersonen();
 }
